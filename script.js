779a580f1ef122d43096a49302d495ce0e4288dc
Roopansh Sethi <38037190+roopansh123@users.noreply.github.com>
2026-05-11 23:52:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent form from refreshing the page
+    event.preventDefault();
 
+    // Get input values
+    let firstName = document.forms["form1"]["fname"].value.trim();
+    let lastName = document.forms["form1"]["lname"].value.trim();
+
+    // Show alert with full name
+    alert(firstName + " " + lastName);
 }
